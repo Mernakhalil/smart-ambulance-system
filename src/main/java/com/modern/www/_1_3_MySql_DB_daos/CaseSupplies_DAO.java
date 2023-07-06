47b0e19170d0ab311a49551e5e79ce671e8b09b3
Mernakhalil <101826533+Mernakhalil@users.noreply.github.com>
2023-07-06 15:45:54 +0300

    Add files via upload

---
+++ src/main/java/com/modern/www/_1_3_MySql_DB_daos/CaseSupplies_DAO.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.modern.www._1_3_MySql_DB_daos;
+
+/**
+ *
+ * @author hp
+ */
+public class CaseSupplies_DAO {
+    
+}
